4010b9706b07156f103a368c29cba7451b664fc2
Daniel Petkevich <daniel.petkevich@gmail.com>
2015-10-01 13:03:59 -0700

    added image paths

--- image_paths.py
+++ image_paths.py
@@ -1,11 +1,11 @@
 
 image_dictionary = {
-	"https://s3-us-west-1.amazonaws.com/benjamingif/main/introowl.gif" : "BQADAwADAgADksGBB3sHRGW-IhYWAg",
-	"https://s3-us-west-1.amazonaws.com/benjamingif/main/dogcar.gif" : "BQADAwADDgADksGBBx_yt3KJGw1jAg",
-	"https://s3-us-west-1.amazonaws.com/benjamingif/main/monkey.gif" : "BQADAwADDwADksGBB9r3UOgIv-WsAg",
-	"https://s3-us-west-1.amazonaws.com/benjamingif/main/owlcashreward.gif" : "BQADAwADEAADksGBB7dXUYRVovgEAg",
-	"https://s3-us-west-1.amazonaws.com/benjamingif/main/owltravel.gif" : "BQADAwADEQADksGBBxZooFxDShtDAg",
-	"https://s3-us-west-1.amazonaws.com/benjamingif/main/student.gif" : "BQADAwADEgADksGBBxULAWehHdl8Ag"
+	"https://s3-us-west-1.amazonaws.com/benjamingif/main/introowl.gif" : "BQADAwADAQADzEd-BTiJxXoiV7NWAg",
+	"https://s3-us-west-1.amazonaws.com/benjamingif/main/dogcar.gif" : "BQADAwADAgADzEd-BauHiDVtrURzAg",
+	"https://s3-us-west-1.amazonaws.com/benjamingif/main/monkey.gif" : "BQADAwADAwADzEd-Bb4lXMdAnuPvAg",
+	"https://s3-us-west-1.amazonaws.com/benjamingif/main/owlcashreward.gif" : "BQADAwADBAADzEd-BYc1CwOeW2n5Ag",
+	"https://s3-us-west-1.amazonaws.com/benjamingif/main/owltravel.gif" : "BQADAwADBQADzEd-BZqEX3aJ8d4dAg",
+	"https://s3-us-west-1.amazonaws.com/benjamingif/main/student.gif" : "BQADAwADBgADzEd-BfC2DI6n7G32Ag"
 
 
 
